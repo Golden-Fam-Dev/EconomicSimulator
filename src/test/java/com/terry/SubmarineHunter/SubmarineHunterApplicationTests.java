d59300d0611019f894888040110f74956f9f3155
bmketbg <bmketbg@gmail.com>
2020-08-11 15:20:17 -0500

    Initial project add

---
+++ src/test/java/com/terry/SubmarineHunter/SubmarineHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terry.SubmarineHunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmarineHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
